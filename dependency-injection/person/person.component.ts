30cb88c3423840c20d1a9baf59aa00bc32470013
Hambric Edwards <hedwar4@live.spcollege.edu>
2020-03-09 19:44:17 -0400

    Add files via upload

---
+++ dependency-injection/person/person.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { InputComponent } from '../input/input.component';
+
+@Component({
+  selector: 'app-person',
+  templateUrl: './person.component.html',
+  styleUrls: ['./person.component.css']
+})
+export class PersonComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
